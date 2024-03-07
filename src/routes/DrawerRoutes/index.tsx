0319e59a268ad8796af927af3e0d9c7e520fe0f0
olDanielS <danielnrt80@gmail.com>
2024-03-07 09:15:17 -0300

    first commit

---
+++ src/routes/DrawerRoutes/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function DrawerRoutes() {
+ return (
+   <View>
+        <Text>Drawer</Text>
+   </View>
+  );
+}
---
+++ src/routes/StackRoutes/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function StackRoutes() {
+ return (
+   <View>
+        <Text>Stack</Text>
+   </View>
+  );
+}
